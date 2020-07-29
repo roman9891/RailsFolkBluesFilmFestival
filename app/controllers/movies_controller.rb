d78083c227a4274d1304ccd0c32529067ecb3e3d
roman9891 <roman9891@gmail.com>
2020-07-29 10:39:07 -0400

    movie controller/model/views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,47 @@
 class MoviesController < ApplicationController
+    before_action :find_movie, only: [:show, :edit, :destroy]
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.create(movie_params)
+        #byebug
+        #if @movie.valid?
+            redirect_to movie_path(@movie)
+        #else
+        #    flash[:errors] = @movie.errors.full_messages
+        #    redirect_to new_movie_path
+        #end
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def find_movie
+        @movie = Movie.find(params[:id])
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:name, :url, :user_id, :is_mature, :acceptance_status)
+    end
 end
