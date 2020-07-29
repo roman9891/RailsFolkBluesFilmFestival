daa3f0134e7988ca3ab7d1e3845dbf1b6a59651d
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-29 09:32:06 -0400

    a lot of messing around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show,:edit,:update,:destroy]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to new_user_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            flash[:errors] = @user.errors.full_messages
+            redirect_to edit_user_path
+        end
+    end
+
+    def destroy
+    end
+
+
+
+
+
+
+    private
+    def user_params
+        params.require(:user).permit(:name,:email,:password)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :movies
     has_many :comments
+
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
 end
