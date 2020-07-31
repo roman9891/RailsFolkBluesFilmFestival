7a469bfffa9e2b1d34c26e552ae16f9a35ccda3c
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-30 20:10:15 -0400

    restarted the base for css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'open-uri'
 
 User.destroy_all
 Movie.destroy_all
@@ -20,12 +21,15 @@ Showing.destroy_all
         email: Faker::Internet.unique.email, 
         password: "funnycat83")
     m1 = Movie.create(
-        title: Faker::JapaneseMedia::SwordArtOnline.game_name, 
+        title: Faker::Movie.title, 
         url: '<iframe src="https://player.vimeo.com/video/209913748?title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         <p><a href="https://vimeo.com/209913748">Adele Radio City</a> from <a href="https://vimeo.com/user7917232">Es Devlin</a> on <a href="https://vimeo.com">Vimeo</a>.</p>', 
         is_mature: true,
         user: User.all.sample,
-        acceptance_status: hero)
+        acceptance_status: hero,
+        )
+       
+        m1.poster.attach(io: open("https://www.fillmurray.com/200/"+(rand(200..220).to_s)), filename: 'file.jpg')
 end
 
 s1 = Showing.create(time: Time.now, movie: Movie.all.sample)
